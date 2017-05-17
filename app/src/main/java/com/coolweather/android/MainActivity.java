10dc251a01abc8133314e2e87416eeccd5f83aa4
oneofnormalprogrammer <13893620653@qq.com>
2017-05-17 16:30:56 +0800

    加入创建数据库和表的各项【配置

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.coolweather;
+package com.coolweather.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
